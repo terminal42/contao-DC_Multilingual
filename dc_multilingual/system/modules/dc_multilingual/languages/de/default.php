c5aa2a7b7d52e8ee7f082bf9c77f605bef6d8a77
Yanick Witschi <yanick.witschi@certo-net.ch>
2011-11-14 17:59:35 +0100

    initial commit

---
+++ dc_multilingual/system/modules/dc_multilingual/languages/de/default.php
@@ -0,0 +1,43 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Leo Feyer 2005-2011
+ * @author     Leo Feyer <http://www.contao.org>
+ * @package    System
+ * @license    LGPL
+ * @filesource
+ */
+
+
+/**
+ * Provide methods to handle multilingual DC_Table entries
+ * @copyright  certo web & design GmbH 2011
+ * @author     Yanick Witschi <yanick.witschi@certo-net.ch>
+ * @package    dc_multilingual
+ */
+$GLOBALS['TL_LANG']['MSC']['editLanguage'] = 'Bearbeiten';
+$GLOBALS['TL_LANG']['MSC']['deleteLanguage'] = 'Löschen';
+$GLOBALS['TL_LANG']['MSC']['defaultLanguage'] = 'Fallback-Sprache';
+$GLOBALS['TL_LANG']['MSC']['editingLanguage'] = 'ACHTUNG: Sie bearbeiten die sprachspezifischen Daten dieses Produkts!';
+$GLOBALS['TL_LANG']['MSC']['deleteLanguageConfirm'] = 'Soll diese Sprache wirklich gelöscht werden? Hierfür gibt es keine Rückgängig-Funktion!';
+$GLOBALS['TL_LANG']['MSC']['undefinedLanguage'] = 'undefiniert';
