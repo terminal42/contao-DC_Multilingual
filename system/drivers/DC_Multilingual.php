e4e7f5558b62935e93f2c58dfe74c47a2910d913
Weyert de Boer <weyert@gmail.com>
2012-02-23 15:44:35 +0100

    Improved the way the versions panel gets shown. Now it only gets shown when versions are available like in the normal DC_Table

--- system/drivers/DC_Multilingual.php
+++ system/drivers/DC_Multilingual.php
@@ -239,8 +239,9 @@ class DC_Multilingual extends DC_Table
 			if (!$objRow->numRows)
 			{
 				// check if the field 'pid' exists in the table, if so we should always store the pid idea as suggested in
-				// DC_Table#getNewPosition-method line 1130
-				if ($this->Database->fieldExists('pid', $this->strTable)) {
+				// DC_Table::getNewPosition(), line 1130
+				if ($this->Database->fieldExists('pid', $this->strTable))
+				{
 					$intId = $this->Database->prepare("INSERT INTO " . $this->strTable . " ({$this->strPidColumn},tstamp,{$this->strLangColumn},pid) VALUES (?,?,?,?)")->execute($this->intId, time(), $_SESSION['BE_DATA']['language'][$this->strTable][$this->intId], CURRENT_ID)->insertId;
 				}
 				else
@@ -498,9 +499,14 @@ class DC_Multilingual extends DC_Table
 		}
 
 
-		$version .= '<div class="clear"></div></div>';
-
-		// Add some buttons and end the form
+		if ( $hasVersions )
+		{
+			$version .= '<div class="clear"></div></div>';
+		} else {
+			$version = '';
+		}
+		
+        // Add some buttons and end the form
 		$return .= '
 </div>
 
