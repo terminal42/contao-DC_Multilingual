604edd616073a6a5bac9220abac95a2ad2f7f8ba
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-01-21 11:31:03 +0100

    Updated copyright statement

--- classes/DC_Multilingual_Query.php
+++ classes/DC_Multilingual_Query.php
@@ -1,11 +1,12 @@
 <?php
 
 /**
- * @copyright     4ward.media 2011 <http://www.4wardmedia.de>
- * @author        Christoph Wiechert <wio@psitrax.de>
- * @package       DC_Multilingual
- * @license       LGPL
- * @filesource
+ * dc_multilingual Extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2011-2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-dc_multilingual
  */
 
 
--- config/autoload.php
+++ config/autoload.php
@@ -1,9 +1,12 @@
 <?php
 
 /**
- * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
- * @package DC_Multilingual
- * @license LGPL
+ * dc_multilingual Extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2011-2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-dc_multilingual
  */
 
 
