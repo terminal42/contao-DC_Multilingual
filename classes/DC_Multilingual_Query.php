2d13744e0f5a2255041166136d924a4018ca0849
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-01-21 11:44:10 +0100

    Removed another namespace declaration

--- classes/DC_Multilingual_Query.php
+++ classes/DC_Multilingual_Query.php
@@ -11,12 +11,6 @@
 
 
 /**
- * Run in a custom namespace so the class can be replaced
- */
-namespace Contao;
-
-
-/**
  * Create a Query for a DC_Multilingual table
  */
 class DC_Multilingual_Query extends Controller
