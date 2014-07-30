de9eeabe18446c2337a39181276115f7a5136aa2
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-07-30 19:12:32 +0200

    Use methods introduced in Contao 3.3

--- models/MultilingualModel.php
+++ models/MultilingualModel.php
@@ -26,92 +26,35 @@ abstract class MultilingualModel extends \Model
 
 
     /**
-     * {@inheritdoc}
+     * Build a query based on the given options
+     *
+     * @param array $arrOptions The options array
+     *
+     * @return string The query string
      */
-    protected static function find(array $arrOptions)
+    protected static function buildFindQuery(array $arrOptions)
     {
         if (static::$blnMultilingual === false) {
             return parent::find($arrOptions);
         }
 
-        if (static::$strTable == '') {
-            return null;
-        }
-
-        $arrOptions['table'] = static::$strTable;
-        $strQuery = \MultilingualQueryBuilder::find($arrOptions);
-
-        $objStatement = \Database::getInstance()->prepare($strQuery);
-
-        // Defaults for limit and offset
-        if (!isset($arrOptions['limit']))
-        {
-            $arrOptions['limit'] = 0;
-        }
-        if (!isset($arrOptions['offset']))
-        {
-            $arrOptions['offset'] = 0;
-        }
-
-        // Limit
-        if ($arrOptions['limit'] > 0 || $arrOptions['offset'] > 0)
-        {
-            $objStatement->limit($arrOptions['limit'], $arrOptions['offset']);
-        }
-
-        $objStatement = static::preFind($objStatement);
-        $objResult = $objStatement->execute($arrOptions['value']);
-
-        if ($objResult->numRows < 1)
-        {
-            return null;
-        }
-
-        $objResult = static::postFind($objResult);
-
-        if ($arrOptions['return'] == 'Model')
-        {
-            $strPk = static::$strPk;
-            $intPk = $objResult->$strPk;
-
-            // Try to load from the registry
-            $objModel = \Model\Registry::getInstance()->fetch(static::$strTable, $intPk);
-
-            if ($objModel !== null)
-            {
-                return $objModel->mergeRow($objResult->row());
-            }
-
-            return new static($objResult);
-        }
-        else
-        {
-            return \Model\Collection::createFromDbResult($objResult, static::$strTable);
-        }
+        return \MultilingualQueryBuilder::find($arrOptions);
     }
 
 
     /**
-     * {@inheritdoc}
+     * Build a query based on the given options to count the number of records
+     *
+     * @param array $arrOptions The options array
+     *
+     * @return string The query string
      */
-    public static function countBy($strColumn=null, $varValue=null)
+    protected static function buildCountQuery(array $arrOptions)
     {
         if (static::$blnMultilingual === false) {
             return parent::count($strColumn, $varValue);
         }
 
-        if (static::$strTable == '')
-        {
-            return 0;
-        }
-
-        $strQuery = \MultilingualQueryBuilder::count(array
-        (
-            'table'  => static::$strTable,
-            'column' => $strColumn,
-            'value'  => $varValue
-        ));
-
-        return (int) \Database::getInstance()->prepare($strQuery)->execute($varValue)->count;
+        return \MultilingualQueryBuilder::count($arrOptions);
     }
 }
