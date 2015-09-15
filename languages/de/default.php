ba63cef9db74254bade7faa274e0c5a077f37606
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-09-15 13:40:26 +0200

    Updated labels

--- languages/de/default.php
+++ languages/de/default.php
@@ -14,9 +14,8 @@
  * Miscellaneous
  */
 $GLOBALS['TL_LANG']['MSC']['editLanguage']          = 'Bearbeiten';
-$GLOBALS['TL_LANG']['MSC']['deleteLanguage']        = 'Löschen';
+$GLOBALS['TL_LANG']['MSC']['deleteLanguage']        = 'Sprache löschen';
 $GLOBALS['TL_LANG']['MSC']['defaultLanguage']       = 'Fallback-Sprache';
 $GLOBALS['TL_LANG']['MSC']['editingLanguage']       = 'ACHTUNG: Sie bearbeiten einen sprachspezifischen Datensatz!';
 $GLOBALS['TL_LANG']['MSC']['deleteLanguageConfirm'] = 'Soll diese Sprache wirklich gelöscht werden? Hierfür gibt es keine Rückgängig-Funktion!';
 $GLOBALS['TL_LANG']['MSC']['undefinedLanguage']     = 'undefiniert';
-
