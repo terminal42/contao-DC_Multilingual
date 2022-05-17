452460201c28793a7282ebd7e1ff47a4abf2db40
David Molineus <david.molineus@netzmacht.de>
2022-05-17 10:07:13 +0200

    Add file headers

--- src/Picker/MultilingualPickerProvider.php
+++ src/Picker/MultilingualPickerProvider.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * dc_multilingual Extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2011-2022, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-dc_multilingual
+ */
+
 namespace Terminal42\DcMultilingualBundle\Picker;
 
 use Contao\CoreBundle\Picker\AbstractTablePickerProvider;
